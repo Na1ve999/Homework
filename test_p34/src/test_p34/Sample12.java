9a7ae8d62904b8539cc79c3dd560dd49d88f3d84
Na1ve999 <ludycool999@gmail.com>
2020-10-14 01:15:05 +0800

    HW1

---
+++ test_p34/src/test_p34/Sample12.java
@@ -0,0 +1,27 @@
+package test_p34;
+
+public class Sample12 
+{
+	public static void main(String[] args)
+	{
+		int num1 = 10;
+		int num2 = 5;
+		
+		System.out.println("num1㎝num2贺笲衡");
+		System.out.println("num1+num2单"+(num1 + num2));
+		System.out.println("num1-num2单"+(num1 - num2));
+		System.out.println("num1*num2单"+(num1 * num2));
+		System.out.println("num1/num2单"+(num1 / num2));
+		System.out.println("num1%num2单"+(num1 % num2));
+		
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		
+		b = a++;
+		c = ++a;
+		
+		System.out.println("琌﹚ぇ患糤┮b"+b);
+		System.out.println("琌﹚ぇ患糤┮c"+c);
+	}
+}
